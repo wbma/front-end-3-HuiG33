70351a64327108087421fdd3ae1cfdfcf8d9e066
HuiG33 <joni.tefke@metropolia.fi>
2018-01-22 15:22:12 +0200

    message

--- src/app/list-media/list-media.component.ts
+++ src/app/list-media/list-media.component.ts
@@ -10,7 +10,8 @@ export class ListMediaComponent implements OnInit {
 
   printThis: string;
   mediaArray: any;
-  thumbNail: any;
+
+  //thumbNail: any;
 
   constructor(public mediaService: MediaService) {
   }
@@ -21,8 +22,22 @@ export class ListMediaComponent implements OnInit {
       console.log(data);
       this.mediaArray = data;
 
-      const temp = this.mediaArray[0].filename.split('.');
-      this.thumbNail = (temp[0] + '-tn320.png');
+      this.mediaArray.map(media => {
+        const temp = media.filename.split('.');
+        const thumbName = temp[0] + '-tn320.png';
+        media.thumbnail = thumbName;
+      });
+
+      console.log(this.mediaArray);
+      
+      /*this.mediaArray.forEach((media, index) => {
+        const temp = media.filename.split('.');
+        const thumbName = temp[0] + '-tn320.png';
+        this.mediaArray[index].thumbnail = thumbName;
+      });*/
+
+      /*const temp = this.mediaArray[0].filename.split('.');
+      this.thumbNail = (temp[0] + '-tn320.png');*/
     });
   }
 
